5692f94b7c6abc62b7f3d8a1f308c1c9a69b421d
rookie-leo <proleonarpa@gmail.com>
2023-02-01 10:43:45 -0300

    initial project setup

---
+++ src/main/java/com/financeiro/resumo/ResumoApplication.java
@@ -0,0 +1,13 @@
+package com.financeiro.resumo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financeiro/resumo/ResumoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financeiro.resumo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
